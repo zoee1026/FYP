ef496c25c3cf7d08fa60c560745fdac1f27a2f96
zoe <87565807+zoee1026@users.noreply.github.com>
2023-02-16 18:31:10 +0800

    debug

--- PointcloudVIsulization.py
+++ PointcloudVIsulization.py
@@ -77,8 +77,11 @@ def GetAllTrainFile():
     # checking
     for i in range(len(label_files)):
         filenametarget=label_files[i].split("\\")[-1].split('.')[0]
-        lidar_filess.extend([file for file in lidar_filess if filenametarget in file])
-    print(len(lidar_files),len(label_files))
+        print(filenametarget)
+        filename=[file for file in lidar_filess if filenametarget in file]
+        if filename:
+            lidar_filess.extend(filename)
+    print(len(lidar_filess),len(label_files))
 
     return [lidar_filess, label_files]
 
@@ -86,7 +89,7 @@ def GetAllTrainFile():
 if __name__ == "__main__":
     lidar_files, label_files = GetAllTrainFile()
     # GetAllTrainFile()
-    print ('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+    print ('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
     print(lidar_files[0],label_files[0])
     PCVisualization(lidar_files[0])
     ReadLabelInOneFile(label_files[0])
