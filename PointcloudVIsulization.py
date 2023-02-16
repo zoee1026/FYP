7124315b8644ac940b3d505c3fe2dc08852e4d33
zoe <87565807+zoee1026@users.noreply.github.com>
2023-02-16 19:29:47 +0800

    debug

--- PointcloudVIsulization.py
+++ PointcloudVIsulization.py
@@ -82,7 +82,7 @@ def GetAllTrainFile():
         filename=[file for file in lidar_files if filenametarget in file]
         if filename:
             lidar_files_match.extend(filename)
-            label_files_match.extend(i)
+            label_files_match.append(i)
 
     print(len(lidar_files_match),len(label_files_match))
     match_data=pd.DataFrame({"label_files":lidar_files_match,"label_files":label_files_match})
