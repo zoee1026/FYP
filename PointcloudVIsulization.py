669993e8bd29ddff6401bacd0691eb74f862852f
zoe <87565807+zoee1026@users.noreply.github.com>
2023-02-16 14:50:43 +0800

    Add: visualization code

--- PointcloudVIsulization.py
+++ PointcloudVIsulization.py
@@ -4,3 +4,79 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
+import json
+import glob
+
+
+PATH = '../../dettrain_20220711'
+Lidar_Path = ''
+Label_Path = ''
+
+
+# match files label
+def PCVisualization(lidarPath):
+    pointcloud = np.fromfile(lidarPath, dtype=np.float32)
+    pointcloud = pointcloud.reshape((-1, 4))
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111, projection='3d')
+
+    xs = pointcloud[:, 0][::20]
+    ys = pointcloud[:, 1][::20]
+    zs = pointcloud[:, 2][::20]
+
+    ax.scatter(xs, ys, zs)
+
+
+def ReadLabelInOneFile(labelPath):
+    with open(labelPath) as json_file:
+        data = json.load(json_file)
+        raw = {}
+        boundingBoxes = data['bounding_boxes']
+        for box in boundingBoxes:
+            for k, v in box.items():
+                if k == 'center':
+                    for kk, vv in v.items():
+                        if kk in raw:
+                            raw[kk].append(vv)
+                        else:
+                            raw[kk] = [vv]
+                else:
+                    if k in raw:
+                        raw[k].append(v)
+                    else:
+                        raw[k] = [v]
+        print(raw)
+       
+
+def GetAllTrainFile():
+    lidar_files = []
+    label_files = []
+    for date in os.listdir(PATH)[1:]:
+        # Get lidar file
+        print(os.path.join(PATH, date, 'Data'))
+        lidar_files.extend(sorted([file
+                                   for path, subdir, files in os.walk(os.path.join(PATH, date, 'Data'))
+                                   for file in glob(os.path.join(path, "*.json"))]))
+
+        label_files.extend(sorted([file
+                                   for path, subdir, files in os.walk(os.path.join(PATH, date, 'Label'))
+                                   for file in glob(os.path.join(path, "*.json"))]))
+
+    # checking
+    for i in lidar_files:
+        for j in label_files:
+            if i.split("\\")[-1] ==j.split("\\")[-1]:
+                continue
+            else :
+                print (i,j)
+                break
+    
+        
+    return [lidar_files,label_files]
+
+
+if __name__ == "__main__":
+    lidar_files, label_files=GetAllTrainFile()
+    PCVisualization(lidar_files[0])
+    ReadLabelInOneFile(label_files[0])
