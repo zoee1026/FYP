b19c0d95682d3e243ccfb04e160a2d1dc425767e
zoe <87565807+zoee1026@users.noreply.github.com>
2023-02-16 22:23:31 +0800

    debug

--- PointcloudVIsulization.py
+++ PointcloudVIsulization.py
@@ -84,8 +84,8 @@ def GetAllTrainFile():
             lidar_files_match.extend(filename)
 
     print(len(lidar_files_match),len(label_files_match))
-    match_data=pd.DataFrame({"lidar_files":lidar_files_match,"label_files":label_files_match})
-    match_data.to_csv('MatchFileFeb16.csv')
+    # match_data=pd.DataFrame({"lidar_files":lidar_files_match,"label_files":label_files_match})
+    # match_data.to_csv('MatchFileFeb16.csv')
 
     return [lidar_files_match, label_files_match]
 
@@ -94,8 +94,7 @@ def GetMatchedDatafile(Path):
     return [df['lidar_files'].tolist(),df['label_files'].tolist()]
 
 if __name__ == "__main__":
-    lidar_files, label_files = GetAllTrainFile()
-    print ('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+    # lidar_files, label_files = GetAllTrainFile()
 
     DataPath='MatchFileFeb16.csv'
     lidar_files, label_files = GetMatchedDatafile(DataPath)
@@ -103,6 +102,6 @@ if __name__ == "__main__":
     # GetAllTrainFile()
     print ('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
     print(lidar_files[0],label_files[0])
-    # PCVisualization(lidar_files[0])
-    ReadLabelInOneFile(label_files[0])
+    PCVisualization(lidar_files[0])
+    # ReadLabelInOneFile(label_files[0])
 # 
