8ddcf9abc9fdf1ea0b729866fce27e1f329de05f
zoe <87565807+zoee1026@users.noreply.github.com>
2023-02-16 17:17:06 +0800

    debug

--- PointcloudVIsulization.py
+++ PointcloudVIsulization.py
@@ -63,9 +63,8 @@ def GetAllTrainFile():
         # for path, subdir, files in os.walk(os.path.join(PATH, date, 'Data')):
         #     if not subdir:
         #         print (files)
-        print([file
-               for path, subdir, files in os.walk(os.path.join(PATH, date, 'Data'))
-               for file in glob(os.path.join(path, "*.bin"))])
+        print([files
+               for path, subdir, files in os.walk(os.path.join(PATH, date, 'Data')) if not subdir])
 
     #     lidar_files.extend(sorted([file
     #                                for path, subdir, files in os.walk(os.path.join(PATH, date, 'Data'))
