eccc8d5ab20dcd79b09bf149b3fb1c8b309cdfb5
zoe <87565807+zoee1026@users.noreply.github.com>
2023-02-16 17:31:07 +0800

    debug

--- PointcloudVIsulization.py
+++ PointcloudVIsulization.py
@@ -52,41 +52,35 @@ def ReadLabelInOneFile(labelPath):
 def GetAllTrainFile():
     lidar_files = []
     label_files = []
-    for date in os.listdir(PATH)[1:]:
+    # for date in os.listdir(PATH):
+    for date in  ['south_gate_1680_8Feb2022','south_gate_2Dec2020']:
         # Get lidar file
         # print(os.path.join(PATH, date, 'Data'))
         # print(os.listdir(os.path.join(PATH, date)))
-        print (date)
-        print(os.listdir(os.path.join(PATH, date)))
 
-        # print(os.listdir(os.path.join(PATH, date, 'Data')))
-        # for path, subdir, files in os.walk(os.path.join(PATH, date, 'Data')):
-        #     if not subdir:
-        # #         print (files)
-        # print([files
-        #        for path, subdir, files in os.walk(os.path.join(PATH, date, 'Data')) if not subdir])
+        lidar_files.extend(sorted([file
+                                   for path, subdir, files in os.walk(os.path.join(PATH, date, 'Data'))
+                                   for file in glob(os.path.join(path, "*.bin"))]))
 
-    #     lidar_files.extend(sorted([file
-    #                                for path, subdir, files in os.walk(os.path.join(PATH, date, 'Data'))
-    #                                for file in glob(os.path.join(path, "*.bin"))]))
+        label_files.extend(sorted([file
+                                   for path, subdir, files in os.walk(os.path.join(PATH, date, 'Label'))
+                                   for file in glob(os.path.join(path, "*bin.json"))]))
 
-    #     label_files.extend(sorted([file
-    #                                for path, subdir, files in os.walk(os.path.join(PATH, date, 'Label'))
-    #                                for file in glob(os.path.join(path, "*bin.json"))]))
+    print(len(lidar_files),len(label_files))
+    print('-----------------------------------------------------------------')
+    # checking
+    for i in lidar_files:
+        for j in label_files:
+            if i.split("\\")[-1] == j.split("\\")[-1]:
+                continue
+            else:
+                print(i, j)
+                break
 
-    # # checking
-    # for i in lidar_files:
-    #     for j in label_files:
-    #         if i.split("\\")[-1] == j.split("\\")[-1]:
-    #             continue
-    #         else:
-    #             print(i, j)
-    #             break
-
-    # return [lidar_files, label_files]
+    return [lidar_files, label_files]
 
 
 if __name__ == "__main__":
     lidar_files, label_files = GetAllTrainFile()
-    # PCVisualization(lidar_files[0])
-    # ReadLabelInOneFile(label_files[0])
+    PCVisualization(lidar_files[0])
+    ReadLabelInOneFile(label_files[0])
