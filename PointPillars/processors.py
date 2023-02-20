c519696d7e49fe70346691a99ef4b397e1b282bb
zoe <87565807+zoee1026@users.noreply.github.com>
2023-02-20 21:14:54 +0800

    remove debug msg

--- PointPillars/processors.py
+++ PointPillars/processors.py
@@ -137,10 +137,7 @@ class DataProcessor(Parameters):
         target_dimension = np.array([label.dimension for label in labels], dtype=np.float32)
         target_yaw = np.array([label.yaw for label in labels], dtype=np.float32)
         target_class = np.array([self.classes[label.classification] for label in labels], dtype=np.int32)
-        print(target_class)
-        print(target_positions)
-        print(target_yaw)
-        print(target_dimension)
+
         # assert np.all(target_yaw >= -np.pi) & np.all(target_yaw <= np.pi)
         assert len(target_positions) == len(target_dimension) == len(target_yaw) == len(target_class)
 
