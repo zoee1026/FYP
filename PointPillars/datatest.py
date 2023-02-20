c393033b304ff01cd17622f941dc48012b89ca3d
zoe <87565807+zoee1026@users.noreply.github.com>
2023-02-20 13:42:46 +0800

    data path

--- PointPillars/datatest.py
+++ PointPillars/datatest.py
@@ -1,10 +1,14 @@
 import pandas as pd
 from read_file_location import GetMatchedDatafile
+import glob
+import os
 
-Path='../MatchFile.csv'
-
-df = pd.read_csv(Path)
-print(df.describe)
-
+# Path='../MatchFile.csv'
+trainPath= "/media/sdb1/kitti/object/training"
+lidar_files = sorted(glob(os.path.join(trainPath, "velodyne", "*.bin")))
+label_files = sorted(glob(os.path.join(trainPath, "label_2", "*.txt")))
+calibration_files = sorted(glob(os.path.join(trainPath, "calib", "*.txt")))
+# df = pd.read_csv(Path)
+print (len(label_files),len(lidar_files), len(calibration_files))
 
 
