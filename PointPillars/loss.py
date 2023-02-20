a6ab86299349efd14a98649257abd295aee70f8a
zoe <87565807+zoee1026@users.noreply.github.com>
2023-02-20 20:27:06 +0800

    Revert "updateversion"
    This reverts commit 623830f2af83c9d2bb0f61a887ed046a713e517f.

--- PointPillars/loss.py
+++ PointPillars/loss.py
@@ -22,7 +22,7 @@ class PointPillarNetworkLoss:
     def focal_loss(self, y_true: tf.Tensor, y_pred: tf.Tensor):
         """ y_true value from occ in {-1, 0, 1}, i.e. {bad match, neg box, pos box} """
 
-        self.mask = tf.equal(y_true, 1.)
+        self.mask = tf.equal(y_true, 1)
 
         cross_entropy = K.binary_crossentropy(y_true, y_pred)
 
@@ -35,7 +35,7 @@ class PointPillarNetworkLoss:
         focal_loss = gamma_factor * alpha_factor * cross_entropy
 
         neg_mask = tf.equal(y_true, 0)
-        thr = tfp.stats.percentile(tf.boolean_mask(focal_loss, neg_mask), 90.) # changed percentile from 90 to 80
+        thr = tfp.stats.percentile(tf.boolean_mask(focal_loss, neg_mask), 90.)
         hard_neg_mask = tf.greater(focal_loss, thr)
         # mask = tf.logical_or(tf.equal(y_true, 0), tf.equal(y_true, 1))
         mask = tf.logical_or(self.mask, tf.logical_and(neg_mask, hard_neg_mask))
@@ -61,18 +61,9 @@ class PointPillarNetworkLoss:
         masked_loss = tf.boolean_mask(loss, mask)
         return self.size_weight * tf.reduce_mean(masked_loss)
 
-    def add_sin_difference(self, y_true, y_pred, factor=1.0):
-        if factor != 1.0:
-            y_true = factor * y_true
-            y_pred = factor * y_pred
-        rad_pred_encoding = tf.math.sin(y_pred) * tf.math.cos(y_true)
-        rad_tg_encoding = tf.math.cos(y_pred) * tf.math.sin(y_true)
-        return rad_tg_encoding, rad_pred_encoding
-
     def angle_loss(self, y_true: tf.Tensor, y_pred: tf.Tensor):
-        y_true, y_pred = self.add_sin_difference(y_true, y_pred)
         loss = tf.compat.v1.losses.huber_loss(y_true,
-                                    y_pred, delta=3.0,
+                                    y_pred,
                                     reduction="none")
 
         masked_loss = tf.boolean_mask(loss, self.mask)
