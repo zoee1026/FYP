f00762127c57991453c4e03221776ed235e99dfc
zoe <87565807+zoee1026@users.noreply.github.com>
2023-02-20 13:46:43 +0800

    debug

--- PointPillars/datatest.py
+++ PointPillars/datatest.py
@@ -5,9 +5,9 @@ import os
 
 # Path='../MatchFile.csv'
 trainPath= "/media/sdb1/kitti/object/training"
-lidar_files = sorted(glob(os.path.join(trainPath, "velodyne", "*.bin")))
-label_files = sorted(glob(os.path.join(trainPath, "label_2", "*.txt")))
-calibration_files = sorted(glob(os.path.join(trainPath, "calib", "*.txt")))
+lidar_files = sorted(glob.glob(os.path.join(trainPath, "velodyne", "*.bin")))
+label_files = sorted(glob.glob(os.path.join(trainPath, "label_2", "*.txt")))
+calibration_files = sorted(glob.glob(os.path.join(trainPath, "calib", "*.txt")))
 # df = pd.read_csv(Path)
 print (len(label_files),len(lidar_files), len(calibration_files))
 
