235b4533d15fe9375a77452f55d8f74ea0ee8e41
zoe <87565807+zoee1026@users.noreply.github.com>
2023-03-09 14:24:18 +0800

    add model path and val

--- PointPillars/point_pillars_training_run.py
+++ PointPillars/point_pillars_training_run.py
@@ -1,14 +1,18 @@
 import os
+import sys
 import time
+import logging
+import argparse
 import numpy as np
 import tensorflow as tf
-from glob import glob
+from easydict import EasyDict as edict
 
 from config import Parameters
 from loss import PointPillarNetworkLoss
 from network import build_point_pillar_graph
 from processors import SimpleDataGenerator
 from readers import KittiDataReader
+
 from read_file_location import GetMatchedDatafile, TestModel
 
 tf.get_logger().setLevel("ERROR")
@@ -21,15 +25,29 @@ MODEL_PATH = "model.h5"
 MODEL_SAVE = "train.h5"
 
 
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+def generate_config_from_cmd_args():
+    parser = argparse.ArgumentParser(description='PointPillars training')
+    parser.add_argument('--gpu_idx', default=3, type=int, required=False, 
+        help='GPU index to use for inference')
+    parser.add_argument('--model_root', default='./log/', required=True,
+        help='Path for dumping training logs and model weights')
+    
+    configs = edict(vars(parser.parse_args()))
+    return configs
 
-if __name__ == "__main__":
+
+def train_PillarNet(configs):
 
     params = Parameters()
 
     pillar_net = build_point_pillar_graph(params)
-    # pillar_net.load_weights(os.path.join(MODEL_ROOT, MODEL_PATH))
+    pillar_net.load_weights(os.path.join(MODEL_ROOT, MODEL_PATH))
+
+    if os.path.exists(os.path.join(configs.model_root, "model.h5")):
+        logging.info("Using pre-trained weights found at path: {}".format(configs.model_root))
+        pillar_net.load_weights('zoe_pointpillars.h')
+    else:
+        logging.info("No pre-trained weights found. Initializing weights and training model.")
 
     loss = PointPillarNetworkLoss(params)
 
@@ -39,25 +57,25 @@ if __name__ == "__main__":
 
     data_reader = KittiDataReader()
 
-    lidar_files, label_files = GetMatchedDatafile(DATA_ROOT)
-    # lidar_files, label_files = TestModel(DATA_ROOT)
-    print(len(lidar_files),len(label_files),'---------------------------------------------------------------------')
+    lidar_train, label_train,lidar_val, label_val = GetMatchedDatafile(DATA_ROOT)
+    print(len(lidar_train),len(label_train),'---------------------------------------------------------------------')
+    print(len(lidar_val),len(label_val),'---------------------------------------------------------------------')
 
 
     # "Input dirs require equal number of files."
-    assert len(lidar_files) == len(label_files)
-    # validation_len = int(0.3*len(label_files))
+    assert len(lidar_train) == len(label_train)
+    assert len(lidar_val) == len(label_val)
+
   
-    # training_gen = SimpleDataGenerator(data_reader, params.batch_size, lidar_files[:-validation_len], label_files[:-validation_len])
-    # validation_gen = SimpleDataGenerator(data_reader, params.batch_size, lidar_files[-validation_len:], label_files[-validation_len:])
-    training_gen = SimpleDataGenerator(data_reader, params.batch_size, lidar_files, label_files)
+    training_gen = SimpleDataGenerator(data_reader, params.batch_size, lidar_train, label_train)
+    validation_gen = SimpleDataGenerator(data_reader, params.batch_size, lidar_val, label_val)
 
 
     log_dir = MODEL_ROOT
-    epoch_to_decay = int(
-        np.round(params.iters_to_decay / params.batch_size * int(np.ceil(float(len(label_files)) / params.batch_size))))
+    epoch_to_decay = int( np.round(params.iters_to_decay / params.batch_size * int(np.ceil(float(len(label_train)+len(label_val)) / params.batch_size))))
+       
     callbacks = [
-        tf.keras.callbacks.TensorBoard(log_dir=log_dir),
+        tf.keras.callbacks.TensorBoard(log_dir=log_dir, update_freq=10),
         tf.keras.callbacks.ModelCheckpoint(filepath=os.path.join(log_dir,MODEL_SAVE ),
                                            monitor='val_loss', save_best_only=True),
         tf.keras.callbacks.LearningRateScheduler(
@@ -67,15 +85,30 @@ if __name__ == "__main__":
 
     try:
         pillar_net.fit(training_gen,
-                    #    validation_data = validation_gen,
+                       validation_data = validation_gen,
                        steps_per_epoch=len(training_gen),
                        callbacks=callbacks,
                        use_multiprocessing=True,
-                       epochs=int(params.total_training_epochs),
+                    #    epochs=int(params.total_training_epochs),
+                       epochs=1,
                        workers=6)
+        pillar_net.save('my_model')
+        pillar_net.save('zoe_pointpillars.h')
+
     except KeyboardInterrupt:
         model_str = "interrupted_%s.h5" % time.strftime("%Y%m%d-%H%M%S")
-        # pillar_net.save(os.path.join(log_dir, model_str))
-        # print("Interrupt. Saving output to %s" % os.path.join(os.getcwd(), log_dir[1:], model_str))
+        pillar_net.save(os.path.join(log_dir, model_str))
+        print("Interrupt. Saving output to %s" % os.path.join(os.getcwd(), log_dir[1:], model_str))
         print('<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
         print(model_str)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - [%(levelname)s]: %(message)s")
+    train_configs = generate_config_from_cmd_args()
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(train_configs.gpu_idx)
+    # tf.get_logger().setLevel("ERROR")
+
+    logging.info("Model training logs and trained weights will be saved at path: {}".format(train_configs.model_root))
+    train_PillarNet(train_configs)
--- PointPillars/read_file_location.py
+++ PointPillars/read_file_location.py
@@ -2,13 +2,18 @@ import pandas as pd
 
 def GetMatchedDatafile(Path):
     df=pd.read_csv(Path)
-    train = df.sample(frac=0.8).reset_index(drop=True)
+    train = df.sample(frac=0.9).reset_index(drop=True)
     test = df.drop(train.index)
     train.to_csv('train.csv')
     test.to_csv('test.csv')
-    return [train['lidar_files'].tolist(),train['label_files'].tolist()]
+    return [train['lidar_files'].tolist(),train['label_files'].tolist()/
+            test['lidar_files'].tolist(),test['label_files'].tolist()
+            ]
+
 
 def TestModel(Path):
     df=pd.read_csv(Path)
     df = df.sample(frac=0.5).reset_index(drop=True)
     return [df['lidar_files'].tolist(),df['label_files'].tolist()]
+
+
