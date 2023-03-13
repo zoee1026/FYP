28dd2d675a98c0b0d90073b03052cda59db9ab8b
zoe <87565807+zoee1026@users.noreply.github.com>
2023-03-13 09:35:50 +0800

    try open3d

--- PointPillars/VisMy.py
+++ PointPillars/VisMy.py
@@ -1,19 +1,25 @@
-# import open3d
-# import matplotlib
-# import numpy as np
+import open3d as o3d
+import matplotlib
+import numpy as np
 
-# from read_file_location import ReadFileFromPath
-# from open3dvis import draw_scenes
+from read_file_location import ReadFileFromPath
+from open3dvis import draw_scenes
 
-# if __name__ == '__main__':
-#     lidar_files, label_files = ReadFileFromPath('test.csv')
-#     draw_scenes(lidar_files[0])
+if __name__ == '__main__':
+    lidar_files, label_files = ReadFileFromPath('test.csv')
+    print(lidar_files[0])
+    # draw_scenes(lidar_files[0])
+    o3d.visualization.webrtc_server.enable_webrtc()
+    cube_red = o3d.geometry.TriangleMesh.create_box(1, 2, 4)
+    cube_red.compute_vertex_normals()
+    cube_red.paint_uniform_color((1.0, 0.0, 0.0))
+    o3d.visualization.draw(cube_red)
 
-from xvfbwrapper import Xvfb
-vdisplay = Xvfb(width=1920, height=1080)
-vdisplay.start()
+# from xvfbwrapper import Xvfb
+# vdisplay = Xvfb(width=1920, height=1080)
+# vdisplay.start()
 
-from mayavi import mlab
-mlab.init_notebook('x3d', 800, 450)
-s = mlab.test_plot3d()
-s
+# from mayavi import mlab
+# mlab.init_notebook('x3d', 800, 450)
+# s = mlab.test_plot3d()
+# s
