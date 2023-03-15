41781d29e1b35c469ecd62e5c5aa34a7c4cdc0d5
zoe <87565807+zoee1026@users.noreply.github.com>
2023-03-15 09:23:32 +0800

    fixed path

--- utiliti/TransformedPointCloud.py
+++ utiliti/TransformedPointCloud.py
@@ -10,7 +10,7 @@ PolygonPath=[os.path.join(CutPath, x) for x in os.listdir(CutPath)]
 Valid='/media/sdb1/zoe/FYP/tune_lidar/valid_polygon1.csv'
 ToDir='/media/sdb1/zoe/FYP/train_files/'
 DataPath='/media/sdb1/zoe/FYP/folder_root/TestFile.csv'
-SaveCleanFile='/media/sdb1/zoe/FYP/folder_root/folder_root/CleanedFiles.csv'
+SaveCleanFile='/media/sdb1/zoe/FYP/folder_root/CleanedFiles.csv'
 TransformedFiles= os.listdir(ToDir)
 
 def GetTransformMatrix():
