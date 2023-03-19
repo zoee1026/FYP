9c151d4a63cb5de5c957028ab92ee01d276a4a9b
zoee1026 <kinyan1026@gmail.com>
2023-03-19 19:51:12 +0800

    get Mar18 training detail

--- PointPillars/open3dvisEval.py
+++ PointPillars/open3dvisEval.py
@@ -21,14 +21,29 @@ def ReadLabelInOneFile(labelPath):
 
         print(elements)  
         return np.array(elements)
+    
+def ReadResultInOneFile(labelPath):
+    with open(labelPath) as json_file:
+        data = json.load(json_file)
+        elements = []
+        boundingBoxes = data['bounding_boxes']
+
+        for box in boundingBoxes:
+            if box["object_id"] not in list(VehicaleClasses.keys()):
+                continue
+            element =list(box['center'].values())+[box['width'],box['length'],box['height']]+[str(box['angle']), str(box["object_id"])]
+            elements.append(element)
+
+        print(elements)  
+        return np.array(elements)
 
 if __name__ == '__main__':
     # PointPath=r'C:\Users\Chan Kin Yan\Documents\GitHub\FYP\train_files\2020_12_03=00_03_35_387.bin'
     # LabelPath=r"C:\Users\Chan Kin Yan\Documents\GitHub\FYP\PointPillars\eval\eval_label\2020_12_03=00_03_35_387.bin.json"
     PointPath=r'C:\Users\Chan Kin Yan\Documents\GitHub\FYP\PointPillars\eval\eval_lidar\2020_12_03=00_03_33_388.bin'
     LabelPath=r"C:\Users\Chan Kin Yan\Documents\GitHub\FYP\PointPillars\eval\eval_label\2020_12_03=00_03_33_388.bin.json"
-    elements=ReadLabelInOneFile(LabelPath)
-    labels=[VehicaleClasses[x] for x in list(elements[:,-1])]
+    label_elements=ReadLabelInOneFile(LabelPath)
+    labels=[VehicaleClasses[x] for x in list(label_elements[:,-1])]
 
-    draw_scenes(PointPath=PointPath,transform=True,gt_boxes=elements, ref_labels=labels)
+    draw_scenes(PointPath=PointPath,transform=True,gt_boxes=label_elements, ref_labels=labels)
 
