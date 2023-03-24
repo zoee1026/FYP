d0e7054fd91282753a36518630a2229a59eaa6eb
zoe <87565807+zoee1026@users.noreply.github.com>
2023-03-24 13:30:17 +0800

    debug

--- PointPillars/config.py
+++ PointPillars/config.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 
-# Anchor_file='/media/sdb1/zoe/FYP/folder_root/ZoeAnchor.csv'
-Anchor_file=r'C:\Users\Chan Kin Yan\Documents\GitHub\FYP\folder_root\ZoeAnchor.csv'
+Anchor_file='/media/sdb1/zoe/FYP/folder_root/ZoeAnchor.csv'
+# Anchor_file=r'C:\Users\Chan Kin Yan\Documents\GitHub\FYP\folder_root\ZoeAnchor.csv'
 
 VehicaleClasses = {
 
