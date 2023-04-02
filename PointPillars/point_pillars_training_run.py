ca5adddb36dce3181128a42f5baa98a1ce8ed3c8
zoe <87565807+zoee1026@users.noreply.github.com>
2023-04-02 17:35:35 +0800

    try 2 gpu

--- PointPillars/point_pillars_training_run.py
+++ PointPillars/point_pillars_training_run.py
@@ -97,7 +97,7 @@ def train_PillarNet():
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - [%(levelname)s]: %(message)s")
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-    os.environ["CUDA_VISIBLE_DEVICES"] = '2','3'
+    os.environ["CUDA_VISIBLE_DEVICES"] = '2,3'
     # CUDA_VISIBLE_DEVICES=0,1
     # tf.get_logger().setLevel("ERROR")
     train_PillarNet()
