032bc57d6b469f85c26b02e10f6dc4816017f84d
zoe <87565807+zoee1026@users.noreply.github.com>
2023-04-03 22:50:13 +0800

    config 0.2 m per pillars

--- PointPillars/config.py
+++ PointPillars/config.py
@@ -65,21 +65,21 @@ OutPutVehecleClasees = {
 
 
 class GridParameters:
-    x_min = -50.4
-    x_max = 23.52
-    x_step = 0.28
-
-    y_min = -43.68
-    y_max = 70.56
-    y_step = 0.28
-
     # x_min = -50.4
-    # x_max = 30.24
-    # x_step = 0.16
+    # x_max = 23.52
+    # x_step = 0.28
 
-    # y_min = -33.6
-    # y_max = 47.04
-    # y_step = 0.16
+    # y_min = -43.68
+    # y_max = 70.56
+    # y_step = 0.28
+
+    x_min = -50.4
+    x_max = 30.24
+    x_step = 0.2
+
+    y_min = -33.6
+    y_max = 47.04
+    y_step = 0.2
 
     # x_min = -50.4
     # x_max = 23.52
