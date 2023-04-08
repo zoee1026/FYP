9e20d2230456ee83124225dd10fcf83fb6f2ef0e
zoe <87565807+zoee1026@users.noreply.github.com>
2023-04-08 23:27:42 +0800

    remove debug

--- PointPillars/src/point_pillars.cpp
+++ PointPillars/src/point_pillars.cpp
@@ -643,8 +643,8 @@ std::tuple<pybind11::array_t<float>, int, int> createPillarsTarget(const pybind1
 
     std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
-    // if (printTime)
-    std::cout << "createPillarsTarget took: " << static_cast<float>(duration) / 1e6 << " seconds" << std::endl;
+    if (printTime)
+        std::cout << "createPillarsTarget took: " << static_cast<float>(duration) / 1e6 << " seconds" << std::endl;
 
     return std::make_tuple(tensor, posCnt, negCnt);
 }
