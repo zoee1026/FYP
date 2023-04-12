df9f2613f7959ca082bd280390bbf953e11a9961
zoe <87565807+zoee1026@users.noreply.github.com>
2023-04-12 17:27:12 +0800

    add res less net

--- PointPillars/nets/network_yolo_concat.py
+++ PointPillars/nets/network_yolo_concat.py
@@ -12,7 +12,7 @@ from keras.layers import (Concatenate, Input, Lambda, UpSampling2D,
                           ZeroPadding2D)
 from keras.models import Model
 
-from nets.CSPdarknet import (C3, DarknetConv2D, DarknetConv2D_BN_SiLU,
+from nets.CSPdarknet_3resnet import (C3, DarknetConv2D, DarknetConv2D_BN_SiLU,
                              darknet_body,compose)
 import tensorflow as tf
 import numpy as np
