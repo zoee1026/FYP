c0e72888cddffdb787393d3a04660cd6a4ab48c9
zoe <87565807+zoee1026@users.noreply.github.com>
2023-04-14 00:47:58 +0800

    debug

--- PointPillars/inference_utils.py
+++ PointPillars/inference_utils.py
@@ -364,6 +364,7 @@ def generate_bboxes_from_pred(occ, pos, siz, ang, hdg, clf, anchor_dims, occ_thr
         real_boxes = np.where(occ == np.amax(occ))
     # Get the indices of the occupancy array
     coordinates = list(zip(real_boxes[0], real_boxes[1], real_boxes[2]))
+    print(len(coordinates))
     # Assign anchor dimensions as original bounding box coordinates which will eventually be changed
     # according to the predicted regression targets
     anchor_dims = anchor_dims
