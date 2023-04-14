ea75fe39af4be2b641644f24d212474518e72178
zoe <87565807+zoee1026@users.noreply.github.com>
2023-04-14 21:40:14 +0800

    yup

--- PointPillars/config.py
+++ PointPillars/config.py
@@ -123,7 +123,7 @@ class NetworkParameters:
 
     max_points_per_pillar = 100
     # max_pillars = 10000
-    max_pillars = 12000
+    max_pillars = 10000
 
     nb_features = 9
     nb_channels = 64
