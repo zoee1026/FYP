90fa296957bf7cc2f55bf0ebc03d9b7205b24c0e
zoe <87565807+zoee1026@users.noreply.github.com>
2023-04-16 17:51:09 +0800

    ar

--- PointPillars/processors.py
+++ PointPillars/processors.py
@@ -240,7 +240,7 @@ class SimpleDataGenerator(DataProcessor, Sequence):
 
         pillars = np.concatenate(pillars, axis=0)
         voxels = np.concatenate(voxels, axis=0)
-        print(np.where(voxels>0))
+        print(np.where(np.array(voxels)>0).shape,'----------------------------------------')
 
         if self.label_files is not None:
             occupancy = np.array(occupancy)
