2b123b627b765037071decf3d80eb4ed44bf83c8
zoe <87565807+zoee1026@users.noreply.github.com>
2023-04-17 23:26:43 +0800

    try old

--- PointPillars/pointpillars_inference.py
+++ PointPillars/pointpillars_inference.py
@@ -46,7 +46,7 @@ precisions = {
     15: [],
 }
 RESULT_LABEL_CSV='/media/sdb1/zoe/FYP/folder_root/Val2.csv'
-MODEL='zoe_pointpillars3.h5'
+MODEL='zoe_pointpillars.h5'
 EVAL_PATH='test.csv'
 MODEL_ROOT = "./log"
 MODEL_SAVE = "train3.h5"
