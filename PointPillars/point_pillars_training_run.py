e018be1400486977e0b6a4d6134aae39b5aa3859
zoe <87565807+zoee1026@users.noreply.github.com>
2023-04-17 23:53:49 +0800

    fix pretrain location

--- PointPillars/point_pillars_training_run.py
+++ PointPillars/point_pillars_training_run.py
@@ -37,12 +37,12 @@ def train_PillarNet():
     pillar_net = build_point_pillar_graph(params)
 
     pretrained= os.path.join(MODEL_ROOT,MODEL_SAVE)
-    if os.path.exists(pretrained):
+    if os.path.exists(zoe_pointpillars):
         # with h5py.File('zoe_pointpillars3', 'r') as f:
         #     pillar_net.load_weights(f)
-        pillar_net.load_weights(pretrained)
+        pillar_net.load_weights(zoe_pointpillars)
         
-        logging.info("Using pre-trained weights found at path: {}".format(pretrained))
+        logging.info("Using pre-trained weights found at path: {}".format(zoe_pointpillars))
         
         print("load")
     else:
