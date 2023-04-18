1ea7162dd13edb79c4f2998fc06130c7fc62cfcc
zoe <87565807+zoee1026@users.noreply.github.com>
2023-04-18 14:05:52 +0800

    try

--- PointPillars/nets/network.py
+++ PointPillars/nets/network.py
@@ -33,7 +33,7 @@ def build_point_pillar_graph(params: Parameters, batch_size: int = Parameters.ba
         return tensor + tf.constant(array, dtype=tf.int32)
 
     if batch_size > 1:
-            corrected_indices = tf.keras.layers.Lambda(lambda t: correct_batch_indices(t, batch_size))(input_indices)
+            corrected_indices = tf.keras.layers.Lambda(lambda t: correct_batch_indices(t, params.batch_size))(input_indices)
     else:
         corrected_indices = input_indices
 
