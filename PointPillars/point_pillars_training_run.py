aaacc9d5a3de10eb338e53fd8d3979e738e4e6c3
zoe <87565807+zoee1026@users.noreply.github.com>
2023-04-21 12:01:38 +0800

    try 1 gpu

--- PointPillars/point_pillars_training_run.py
+++ PointPillars/point_pillars_training_run.py
@@ -129,7 +129,7 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
                         format="%(asctime)s - [%(levelname)s]: %(message)s")
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-    os.environ["CUDA_VISIBLE_DEVICES"] = '2,3'
+    os.environ["CUDA_VISIBLE_DEVICES"] = '3'
     
     # os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
 
