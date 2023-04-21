2e237869685a67b2679d581a939b13b54ecce7be
zoe <87565807+zoee1026@users.noreply.github.com>
2023-04-21 12:18:16 +0800

    do zoe_pointPIllars4 first

--- PointPillars/point_pillars_training_run.py
+++ PointPillars/point_pillars_training_run.py
@@ -11,7 +11,7 @@ from config import Parameters
 from loss import PointPillarNetworkLoss
 from nets.network import build_point_pillar_graph
 # from nets.network_yolo import build_point_pillar_graph
-from nets.network_yolo_basechannel import build_point_pillar_graph
+# from nets.network_yolo_basechannel import build_point_pillar_graph
 # from nets.network_yolo_concat import build_point_pillar_graph
 from processors import SimpleDataGenerator
 from readers import KittiDataReader
@@ -26,12 +26,12 @@ import wandb
 # DATA_ROOT = '..folder_root//MatchFile.csv'
 DATA_ROOT = '/media/sdb1/zoe/FYP/folder_root/All.csv'
 MODEL_ROOT = "./log"
-MODEL_SAVE = "train6.h5"
-pb_MODEL='my_model10'
+MODEL_SAVE = "train4.h5"
+pb_MODEL='my_model8'
 
-zoe_pointpillars='zoe_pp_yolo4.h5'
+# zoe_pointpillars='zoe_pp_yolo4.h5'
 # zoe_pointpillars='zoe_pp_yolo3.h5'
-# zoe_pointpillars = 'zoe_pointpillars4.h5'
+zoe_pointpillars = 'zoe_pointpillars4.h5'
 
 def train_PillarNet():
     
