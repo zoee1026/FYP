bb3f91013e89fb60cda28d252b7013ff58b278c9
zoe <87565807+zoee1026@users.noreply.github.com>
2023-05-10 14:11:38 +0800

    haha

--- yolo/nets/network.py
+++ yolo/nets/network.py
@@ -107,17 +107,17 @@ def build_point_pillar_graph(params: Parameters, batch_size: int = Parameters.ba
     occ = tf.keras.layers.Conv2D(nb_anchors, (1, 1), name="occupancy", activation="sigmoid")(concat)
 
     loc = tf.keras.layers.Conv2D(nb_anchors * 3, (1, 1), name="loc", kernel_initializer=tf.keras.initializers.TruncatedNormal(0, 0.001))(concat)
-    loc = tf.keras.layers.Reshape(tuple(i//2 for i in image_size) + (nb_anchors, 3), name="loc/reshape")(loc)
+    loc = tf.keras.layers.Reshape(tuple(i for i in image_size) + (nb_anchors, 3), name="loc/reshape")(loc)
 
     size = tf.keras.layers.Conv2D(nb_anchors * 3, (1, 1), name="size", kernel_initializer=tf.keras.initializers.TruncatedNormal(0, 0.001))(concat)
-    size = tf.keras.layers.Reshape(tuple(i//2 for i in image_size) + (nb_anchors, 3), name="size/reshape")(size)
+    size = tf.keras.layers.Reshape(tuple(i for i in image_size) + (nb_anchors, 3), name="size/reshape")(size)
 
     angle = tf.keras.layers.Conv2D(nb_anchors, (1, 1), name="angle")(concat)
 
     heading = tf.keras.layers.Conv2D(nb_anchors, (1, 1), name="heading", activation="sigmoid")(concat)
 
     clf = tf.keras.layers.Conv2D(nb_anchors * nb_classes, (1, 1), name="clf", activation="linear")(concat)
-    clf = tf.keras.layers.Reshape(tuple(i // 2 for i in image_size) + (nb_anchors, nb_classes), name="clf/reshape")(clf)
+    clf = tf.keras.layers.Reshape(tuple(i for i in image_size) + (nb_anchors, nb_classes), name="clf/reshape")(clf)
 
     pillar_net = tf.keras.models.Model([input_pillars, input_indices], [occ, loc, size, angle, heading, clf])
     print(pillar_net.summary())
