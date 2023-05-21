8edfd6a24b1a642824a6d87eb4d487c21cf5b3a7
zoe <87565807+zoee1026@users.noreply.github.com>
2023-05-21 17:27:39 +0800

    a

--- yolo/loss_multi.py
+++ yolo/loss_multi.py
@@ -10,7 +10,9 @@ from box_utili import get_anchors_and_decode
 
 def ciou_cal(y_true, y_pre):
     # conf, x, y, z, l, w, h, yaw, [classes]
-    iou = np.vectorize(ciouraw)(K.eval(y_true), K.eval(y_pre))
+    t=K.eval(y_true)
+    p=K.eval(y_pre)
+    iou = np.vectorize(ciouraw)(t,p)
     print(iou.shape)
     return K.variable(iou)
 
