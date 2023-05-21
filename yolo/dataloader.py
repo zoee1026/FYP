9c75c31ab1d5019cf0cdbf8fd4c2d337531657d9
zoe <87565807+zoee1026@users.noreply.github.com>
2023-05-21 12:49:17 +0800

    debug

--- yolo/dataloader.py
+++ yolo/dataloader.py
@@ -33,17 +33,21 @@ def preprocess_true_boxes(labels: List[Label3D]):
     params = Parameters()
 
     num_layers = params.nb_layer
-    input_shape = np.array([params.Xn_f, params.Yn_f], dtype='float32')
+    input_shape = np.array([params.Xn_f, params.Yn_f], dtype='int32')
     grid_shapes = np.array(
-        [input_shape // {0: 2, 1: 1}[l] for l in range(num_layers)], dtype='float32')
+        [input_shape // {0: 2, 1: 1}[l] for l in range(num_layers)], dtype='int32')
+    print('grid shape',grid_shapes.shape)
     anchors_mask = params.anchors_mask
+    print('anchor mask',anchors_mask)
     anchor = params.anchor_dims
     anchor_l = list(anchor[..., 0])
+    print('anchor length',anchor_l)
 
     # centroid*3, loc*3 ,yaw, occu
 
     y_true = [np.zeros((grid_shapes[l][0], grid_shapes[l][1], len(params.anchors_mask[l]), 8 + params.nb_classes),
                        dtype='float32') for l in range(num_layers)]
+    print('y_true shape',y_true.shape)
 
     for label in labels:
         n = closest_anchor(label.centroid[0], anchor_l)
