c210520a19a72f34e5c48c31c2ba2b62d9ab2315
zoe <87565807+zoee1026@users.noreply.github.com>
2023-05-22 17:13:03 +0800

    a

--- yolo/src/point_pillars.cpp
+++ yolo/src/point_pillars.cpp
@@ -475,7 +475,7 @@ float ciouraw(
     anchorBox.height = anchorBoxList.at(5);
     anchorBox.yaw = anchorBoxList.at(6);
 
-    if labelBox.length==0: return 0;
+    if (labelBox.length==0) return 0;
     return ciou(labelBox,anchorBox);
 
 }
