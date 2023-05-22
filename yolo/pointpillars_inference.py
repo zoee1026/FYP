69752de0ee9039829544feb36b2fbad540a8fd78
zoe <87565807+zoee1026@users.noreply.github.com>
2023-05-22 20:46:41 +0800

    a

--- yolo/pointpillars_inference.py
+++ yolo/pointpillars_inference.py
@@ -70,7 +70,7 @@ def generate_config_from_cmd_args():
                         help='Path for dumping result labels in KITTI format')
     parser.add_argument('--model_path', default=MODEL, type=str, required=False,
                         help='Path to the model weights to be used for inference')
-    parser.add_argument('--occ_thresh', default=0.4, type=float, required=False,
+    parser.add_argument('--occ_thresh', default=0.3, type=float, required=False,
                         help='Occlusion threshold for predicted boxes')
     parser.add_argument('--nms_thresh', default=0.3, type=float, required=False,
                         help='IoU threshold for NMS')
